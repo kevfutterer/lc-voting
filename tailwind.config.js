268122d82c67571b117d39fab263eda4e0feb25a
Kevin Futterer <kevin.futterer@gmail.comm>
2024-06-19 13:47:33 +0200

    beginning of project first design

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                'gray-background': '#f7f8fc',
+                'blue': '#328af1',
+                'blue-hover': '#2879bd',
+                'yellow' : '#ffc73c',
+                'red' : '#ec454f',
+                'red-100' : '#fee2e2',
+                'green' : '#1aab8b',
+                'green-50': '#f0fdf4',
+                'purple' : '#8b60ed',
+            },
+            spacing: {
+                70: '17.5rem',
+                175: '43.75rem',
+            },
+            maxWidth: {
+                custom: '62.5rem',
+            },
+            fontFamily: {
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
