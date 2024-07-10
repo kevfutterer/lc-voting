28f4063b67fe19438ccdd893a9f53e903a33c67f
Kevin Futterer <kevin.futterer@gmail.comm>
2024-07-10 13:42:40 +0200

    Idea model and migration added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=Open+Sans:400,500,600&display=swap" rel="stylesheet" />
+        @livewireStyles
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -104,5 +105,6 @@
             </div>
             
         </main>
+        @livewireScripts
     </body>
 </html>
