a4f25d18d02e9b630f5494b94754b3559f672940
Kevin Futterer <kevin.futterer@gmail.comm>
2024-07-15 11:25:03 +0200

    mail to voters for updated status of idea

--- app/Livewire/SetStatus.php
+++ app/Livewire/SetStatus.php
@@ -5,11 +5,14 @@ namespace App\Livewire;
 use App\Models\Idea;
 use Livewire\Component;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\IdeaStatusUpdatedMailable;
 
 class SetStatus extends Component
 {
     public $idea;
     public $status;
+    public $notifyAllVoters;
 
     public function mount(Idea $idea)
     {
@@ -25,7 +28,26 @@ class SetStatus extends Component
 
         $this->idea->status_id = $this->status;
         $this->idea->save();
+
+        if ($this->notifyAllVoters) {
+            $this->notifyAllVoters();
+        }
+
         $this->dispatch('statusWasUpdated');
+    }
+
+    public function notifyAllVoters()
+    {
+        $this->idea->votes()
+            ->select('name', 'email')
+            ->chunk(100, function ($voters) {
+                foreach ($voters as $user) {
+                    Mail::to($user)
+                        ->queue(new IdeaStatusUpdatedMailable($this->idea));
+                }
+            });
+
+        
     }
 
     public function render()
--- resources/views/livewire/set-status.blade.php
+++ resources/views/livewire/set-status.blade.php
@@ -67,7 +67,7 @@
                         </div>
                         <div>
                             <label class="font-normal inline-flex items-center">
-                                <input class="rounded bg-gray-200" type="checkbox" name="notify_voters" checked="" id="">
+                                <input wire:model.live="notifyAllVoters" class="rounded bg-gray-200" type="checkbox" name="notify_voters">
                                 <span class="ml-2">Notify all voters</span>
                             </label>
                         </div>
